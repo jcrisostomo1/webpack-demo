67bf9aed5a148f3137f4765d378076b32227f86f
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-16 13:29:15 -0400

    continue tutorial to asset management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
   
      // Lodash, now imported by this script
     element.textContent = myName("Josh Crisostomo");
+    element.classList.add('hello');
   
     return element;
   }
