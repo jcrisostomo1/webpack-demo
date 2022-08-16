01606d546871d5da9f5b77e6307f0dc9c2ac3c11
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-16 18:34:30 -0400

    add htmlwebpack plugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import myName from './myName';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+	const btn = document.createElement('button');
+
+	btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   
   // Lodash, now imported by this script
-  element.textContent = myName("Josh Crisostomo");
+  //element.textContent = myName("Josh Crisostomo");
 
   return element;
 }
