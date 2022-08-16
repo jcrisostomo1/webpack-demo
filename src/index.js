859f8fc21df57fbabf4b21e87f2493edc092a463
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-16 14:22:04 -0400

    add images to webpack

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import _ from 'lodash';
 import myName from './myName';
+import Icon from './icon.png';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
   
-     // Lodash, now imported by this script
-    element.textContent = myName("Josh Crisostomo");
-    element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.textContent = myName("Josh Crisostomo");
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
   
-    return element;
-  }
+  return element;
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
